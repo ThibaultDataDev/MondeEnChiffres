c71ae70dd50d0237d009bd8841fd22f070abf22a
ThibaultDataDev <67363761+ThibaultDataDev@users.noreply.github.com>
2020-09-02 11:56:45 +0200

    Update 3_insertcountry.sql

--- 3_insertcountry.sql
+++ 3_insertcountry.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE PROCEDURE insertcountry(nom text) AS $$ 
 BEGIN
 INSERT INTO country (name, population, density)
-VALUES (nom, random()*6433243, random()*4433, random()*76632,random()*6433243, random()*4433, random()*76632,random()*6433243, random()*4433, random()*76632);
+VALUES (nom, random()*6433243, random()*4433);
 
 END;
 $$
